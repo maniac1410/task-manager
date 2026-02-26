f44748fe4989b53c1a7b44ab5ce73272795bf950
maniac1410 <shuklasanjesh8@gmail.com>
2026-02-26 12:42:13 +0530

    Add root redirect for production

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.shortcuts import redirect
+
+def root_redirect(request):
+    return redirect('/tasks/')
 
 
 urlpatterns = [
